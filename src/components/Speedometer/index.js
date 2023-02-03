1fffc43f670c018a4f1c6fc15850579ebee794b7
prathyusha5555 <kprathyusha5555@gmail.com>
2023-02-03 11:44:40 +0000

    speedometer Sol

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,60 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class Speedometer extends Component {
+  state = {speed: 0}
+
+  accelerate = () => {
+    const {speed} = this.state
+
+    if (speed < 200) {
+      this.setState(prevState => ({speed: prevState.speed + 10}))
+    }
+  }
+
+  applyBrake = () => {
+    const {speed} = this.state
+
+    if (speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
+  }
+
+  render() {
+    const {speed} = this.state
+    return (
+      <div className="bg-container">
+        <h1 className="heading">SPEEDOMETER</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+          className="image"
+          alt="speedometer"
+        />
+
+        <h1 className="speed-msg">Speed is {speed}mph</h1>
+        <p className="limit-description">
+          Min Limit is 0mph, Max Limit is 200mph
+        </p>
+        <div className="buttons-container">
+          <button
+            className="accelerate-button"
+            type="button"
+            onClick={this.accelerate}
+          >
+            Accelerate
+          </button>
+          <button
+            className="apply-break-button"
+            type="button"
+            onClick={this.applyBrake}
+          >
+            Apply Brake
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Speedometer
